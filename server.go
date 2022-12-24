194ed42d3e53c25868eb0adabd7bf1b73e310dac
chatupon <khochatupon@gmail.com>
2022-12-24 18:46:47 +0700

    init database and go mod

--- server.go
+++ server.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/ciizo/assessment/database"
 )
 
 func main() {
 	fmt.Println("Please use server.go for main file")
 	fmt.Println("start at port:", os.Getenv("PORT"))
+
+	database.InitDB()
 }
