d24610b4f262580e00ea6847a95a8716867390cc
chatupon <khochatupon@gmail.com>
2023-01-07 19:58:06 +0700

    change var name

--- share/variable.go
+++ share/variable.go
@@ -6,4 +6,4 @@ import (
 
 var Validate *validator.Validate
 
-const ITTestDbConnectioString = "postgresql://root:root@db/go-it-db?sslmode=disable"
+const IT_Test_DB_ConnectionString = "postgresql://root:root@db/go-it-db?sslmode=disable"
